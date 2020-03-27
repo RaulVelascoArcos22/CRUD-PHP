71a9b14d89063a20fc70d39ee97cb79255ef16be
raul Velasco Arcos <velascoarcosraul@gmail.com>
2020-03-26 21:52:27 -0600

    Archivos base creados

---
+++ db.php
@@ -0,0 +1,9 @@
+<!-- Conecion ala base de datos -->
+<?php
+$conn = mysqli_connect(
+    'localhost',
+    'root',
+    'codebitra17',
+    'php-crud'
+);
+?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<!-- Coneccion ala base de datos -->
+<?php include("db.php")?>
+<?php include("includes/header.php")?>
+
+<?php include("includes/footer.php")?>
