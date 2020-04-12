ad49773320dbccbf0249286d6227021573067b31
raul Velasco Arcos <velascoarcosraul@gmail.com>
2020-04-11 20:30:09 -0500

    Boton de eliminar funciona

---
+++ delete_task.php
@@ -0,0 +1,14 @@
+
+<?php include("db.php");
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+        $query = "DELETE FROM task where id=$id";
+        $resul = mysqli_query($conn,$query);
+        if(!$resul){
+            die("Query Failed");
+        }
+        $_SESSION['message']='Task Removed Successfully';
+        $_SESSION['message_type']='danger';
+        header("Location: index.php");
+    }
+?>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 <?php include("db.php")?>
 <?php include("includes/header.php")?>
-    <div class="container p-4">
+    <main class="container p-4">
+        <div class="row">
         <div class="col-md-4">
             <?php if(isset($_SESSION['message'])){?>
                 <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible" role="alert">
@@ -24,6 +25,33 @@
             </div>
         </div>
         <div class="col-md-8">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Created At</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            $query="SELECT * FROM  task";
+                            $result_tasks = mysqli_query($conn,$query);
+                            while($row = mysqli_fetch_array($result_tasks)){?>
+                                <tr>
+                                    <td><?php echo $row['title']?></td>
+                                    <td><?php echo $row['description']?></td>
+                                    <td><?php echo $row['created_at']?></td>
+                                    <td>
+                                        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary "><i class="fas fa-edit "></i> </a>
+                                        <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger "><i class="far fa-trash-alt "></i> </a>
+                                    </td>
+                                </tr> 
+                            <?php } ?>
+                    </tbody>
+                </table>
         </div>
-    </div>
+        </div>
+    </main>
 <?php include("includes/footer.php")?>
